a19587b90450644b4c315724a9dbe5d246a34442
Aakash Sharma <aakashsharma121269@gmail.com>
2026-04-21 23:53:17 +0530

    Create arrayleaders.java

---
+++ TCS/arrayleaders.java
@@ -0,0 +1,17 @@
+class Solution {
+    static ArrayList<Integer> leaders(int arr[]) {
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+        int max = Integer.MIN_VALUE;
+        
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] >= max) {
+                ans.add(arr[i]);
+                max = arr[i];
+            }
+        }
+        
+        Collections.reverse(ans);
+        return ans;
+    }
+}
