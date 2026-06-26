f3adef914d5e87f23601e2fc42441005981efa5f
Aakash Sharma <aakashsharma121269@gmail.com>
2026-06-26 23:54:48 +0530

    Create prime.py

---
+++ TCS/prime.py
@@ -0,0 +1,11 @@
+import math
+n = int(input())
+if n < 2:
+print('Not Prime')
+else:
+is_prime = True
+for i in range(2, int(math.sqrt(n)) + 1):
+if n % i == 0:
+is_prime = False
+break
+print('Prime' if is_prime else 'Not Prime')
